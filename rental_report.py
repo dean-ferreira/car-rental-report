45457cfaad8ead0066e08a9e81194e8b19858522
Dean Ferreira <deanferreira20@gmail.com>
2023-09-29 09:44:55 -0500

    Import SummaryReport

--- rental_report.py
+++ rental_report.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python3
-from json import load
+import datetime
 import sys
+from json import load
 
+from app.reportgenerator.src.summary_report import SummaryReport
 from app.reportgenerator.src.utils import read_json_file
 
 
 def main():
     events = read_json_file(sys.argv[1])
-    print(events)
+    summary_report = SummaryReport()
+    summary_report.store_sessions(events)
+    generated_report = summary_report.generate_report()
+    generated_report.to_csv(f"SR_{datetime.date.today()}.csv")
 
 
 if __name__ == "__main__":
